b3f22c28efadaaf1cd5fc20f854e661de03acb24
Shadowdracarys <ArxSevenArbalest@Gmail.com>
2021-07-18 23:38:57 +0900

    correct

--- chimen/settings/base.py
+++ chimen/settings/base.py
@@ -105,7 +105,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'statices/'),
+    os.path.join(BASE_DIR, 'staticfiles/'),
 ]
 
 MEDIA_URL = '/media/'
